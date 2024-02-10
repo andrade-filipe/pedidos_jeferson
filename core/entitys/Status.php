f87f4819a2ee60a0152d4336adf73b6a97a949e7
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-10 12:23:30 -0300

    Entidade: Status

---
+++ core/entitys/Status.php
@@ -0,0 +1,41 @@
+<?php
+    class Status {
+        private $id;
+        private $status;
+        private $orderId;
+
+        public function getId(){
+            return $this -> id;
+        }
+
+        public function setId($id){
+            $this -> id = $id;
+        }
+
+        public function getStatus(){
+            return $this -> status;
+        }
+
+        public function setStatus($status){
+            $this -> status = $status;
+        }
+
+        public function getOrderId(){
+            return $this -> orderId;
+        }
+
+        public function setOrderId($orderId){
+            $this -> orderId = $orderId;
+        }
+    }
+
+    enum Status {
+        case Novo;
+        case Processamento;
+        case Postado;
+    }
+
+    interface StatusDAO {
+        public function findByOrderId($orderId);
+    }
+?>
---
+++ core/repositorys/StatusRepository.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("../entitys/Status.php");
+
+    class StatusRepository implements StatusDAO {
+        public function findByOrderId($orderId){}
+    }
+?>
