cf294ab0926fdf83c7692707c0e78bedef521877
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-10 21:19:10 -0300

    detalhes da página de formulário

--- application/templates/footer.php
+++ application/templates/footer.php
@@ -1,5 +1,5 @@
-    <footer>
-
+    <footer class="container text-center">
+        <p>Site created by <a href="https://www.linkedin.com/in/filipe-andrade-02777123b/" target="_blank">Filipe Andrade</a> &copy; All Rights Reserved</p>
     </footer>
 </body>
 </html>
