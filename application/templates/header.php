d445643ec4486e7672c7daa049ef993a2b52edb7
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-10 22:04:13 -0300

    criação de um modulo para diminuir os includes

--- application/templates/header.php
+++ application/templates/header.php
@@ -1,9 +1,5 @@
 <?php
-    include_once("infrastructure/global.php");
-    include_once("infrastructure/database.php");
-    include_once("infrastructure/Message.php");
-
-    $message = new Message($BASE_URL);
+    include_once("infrastructure/basicModule.php");
 
     $showMessage = $message -> getMessage();
 
@@ -24,6 +20,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- FONT AWESOME -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind:wght@300;400;700&display=swap" rel="stylesheet">
     <!-- CSS -->
     <link rel="stylesheet" href="<?= $BASE_URL ?>/styles/style.css">
 </head>
---
+++ infrastructure/basicModule.php
@@ -0,0 +1,7 @@
+<?php
+    include_once("infrastructure/global.php");
+    include_once("infrastructure/database.php");
+    include_once("infrastructure/Message.php");
+
+    $message = new Message($BASE_URL);
+?>
