847fa78ef0438218bada4c012378245477430840
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-10 21:33:00 -0300

    footer details

--- application/templates/footer.php
+++ application/templates/footer.php
@@ -1,5 +1,6 @@
-    <footer class="container text-center">
+    <footer class="container d-flex justify-content-between align-items-center col-10">
         <p>Site created by <a href="https://www.linkedin.com/in/filipe-andrade-02777123b/" target="_blank">Filipe Andrade</a> &copy; All Rights Reserved</p>
+        <button class="btn btn-outline-secondary">Login</button>
     </footer>
 </body>
 </html>
