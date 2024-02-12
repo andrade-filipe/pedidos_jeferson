d14d99db102d41c976d208d5a37de6ec1350bece
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-12 16:34:26 -0300

    diminuindo quantidade de dados

--- db_migration/insert_orders.sql
+++ db_migration/insert_orders.sql
@@ -4,8 +4,4 @@ INSERT INTO orders (name, email, order_date, category, content, status) VALUES
 ('Carlos Costa', 'carlos@email.com', NOW(), 'filme', 'Contenido do filme', 'postado'),
 ('Ana Santos', 'ana@email.com', NOW(), 'novela', 'Contenido da novela', 'novo'),
 ('Pedro Fernandes', 'pedro@email.com', NOW(), 'canal', 'Outro conteúdo do canal', 'processamento'),
-('Luísa Pereira', 'luisa@email.com', NOW(), 'serie', 'Outro conteúdo da série', 'postado'),
-('Fernando Cardoso', 'fernando@email.com', NOW(), 'filme', 'Outro conteúdo do filme', 'novo'),
-('Rafael Ribeiro', 'rafael@email.com', NOW(), 'novela', 'Outro conteúdo da novela', 'processamento'),
-('Mariana Ferreira', 'mariana@email.com', NOW(), 'canal', 'Mais um conteúdo do canal', 'postado'),
-('Daniel Mendes', 'daniel@email.com', NOW(), 'serie', 'Mais um conteúdo da série', 'novo');
+('Luísa Pereira', 'luisa@email.com', NOW(), 'serie', 'Outro conteúdo da série', 'postado');
