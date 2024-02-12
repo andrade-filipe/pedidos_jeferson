2d7f881ab94ecfc6a2ca2cebe3bf7c5273555e00
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-12 03:25:20 -0300

    phpmail está funcionando

--- core/services/dashboardService.php
+++ core/services/dashboardService.php
@@ -1,4 +1,7 @@
 <?php
+
+use PHPMailer\PHPMailer\Exception;
+
 include_once("../../infrastructure/global.php");
 include_once("../../infrastructure/database.php");
 include_once("../../infrastructure/Message.php");
@@ -11,76 +14,86 @@ $message = new Message();
 $orderRepository = new OrderRepository($db_connection);
 
 if ($_POST["cancel"]) {
-    cancelProcess($_POST["cancel"], $orderRepository, $mail);
+    cancelProcess($_POST["cancel"], $orderRepository, $mail, $message);
 } else if ($_POST["accept"]) {
-    acceptProcess($_POST["accept"], $orderRepository, $mail);
+    acceptProcess($_POST["accept"], $orderRepository, $mail, $message);
 } else if ($_POST["post"]) {
-    postProcess($_POST["post"], $orderRepository, $mail);
+    postProcess($_POST["post"], $orderRepository, $mail, $message);
 } else if ($_POST["delete"]) {
-    deleteProcess($_POST["delete"], $orderRepository);
+    deleteProcess($_POST["delete"], $orderRepository, $message);
 } else if ($_POST["back"]) {
-    backProcess($_POST["back"], $orderRepository);
+    backProcess($_POST["back"], $orderRepository, $message);
 } else {
     $message->setMessage("Processo não identificado", "error");
 }
 
-function cancelProcess($orderId, OrderRepository $orderRepository, $mail){
+function cancelProcess($orderId, OrderRepository $orderRepository, $mail, $message){
     $orderRepository->deleteOrder($orderId);
     $order = $orderRepository -> findById($orderId);
     $to = $order -> getEmail();
 
-    // Define o remetente
-    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
-    // Define o destinatário
-    $mail->addAddress($to, 'Destinatário');
-    // Conteúdo da mensagem
-    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
-    $mail->Subject = 'Seu Pedido Foi Cancelado';
-    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
-    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
-    // Enviar
-    $mail->send();
+    try{
+        $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+        // Define o destinatário
+        $mail->addAddress($to, 'Destinatário');
+        // Conteúdo da mensagem
+        $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+        $mail->Subject = 'Seu Pedido Foi Cancelado';
+        $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+        $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+        // Enviar
+        $mail->send();
+    } catch(Exception $e) {
+        $message -> setMessage("Erro no envio de email", "error");
+    }
+
 
     header("Location: " . "../../dashboard.php");
 }
 
-function acceptProcess($orderId, OrderRepository $orderRepository, $mail){
+function acceptProcess($orderId, OrderRepository $orderRepository, $mail, $message){
     $orderRepository->updateOrderStatus($orderId, "processamento");
     $order = $orderRepository -> findById($orderId);
 
     $to = $order -> getEmail();
 
-    // Define o remetente
-    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
-    // Define o destinatário
-    $mail->addAddress($to, 'Destinatário');
-    // Conteúdo da mensagem
-    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
-    $mail->Subject = 'Seu Pedido Foi Aprovado';
-    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
-    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
-    // Enviar
-    $mail->send();
+    try{
+        $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+        // Define o destinatário
+        $mail->addAddress($to, 'Destinatário');
+        // Conteúdo da mensagem
+        $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+        $mail->Subject = 'Seu Pedido Foi Aprovado';
+        $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+        $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+        // Enviar
+        $mail->send();
+    } catch (Exception $e){
+        $message -> setMessage("Erro no envio de email", "error");
+    }
 
     header("Location: " . "../../dashboard.php");
 }
 
-function postProcess($orderId, OrderRepository $orderRepository, $mail){
+function postProcess($orderId, OrderRepository $orderRepository, $mail, $message){
     $orderRepository->updateOrderStatus($orderId, "postado");
     $order = $orderRepository -> findById($orderId);
     $to = $order -> getEmail();
 
-    // Define o remetente
-    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
-    // Define o destinatário
-    $mail->addAddress($to, 'Destinatário');
-    // Conteúdo da mensagem
-    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
-    $mail->Subject = 'Seu Pedido Foi Postado';
-    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
-    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
-    // Enviar
-    $mail->send();
+    try {
+        $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+        // Define o destinatário
+        $mail->addAddress($to, 'Destinatário');
+        // Conteúdo da mensagem
+        $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+        $mail->Subject = 'Seu Pedido Foi Postado';
+        $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+        $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+        // Enviar
+        $mail->send();
+    } catch (Exception $e){
+        $message -> setMessage("Erro no envio de email", "error");
+    }
 
     header("Location: " . "../../dashboard.php");
 }
--- infrastructure/mail.php
+++ infrastructure/mail.php
@@ -1,4 +1,5 @@
 <?php
+    include("Message.php");
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
     require 'path/to/PHPMailer/src/Exception.php';
@@ -6,13 +7,18 @@
     require 'path/to/PHPMailer/src/SMTP.php';
 
     $mail = new PHPMailer(true);
+    $message = new Message();
 
-    $mail->isSMTP();        //Devine o uso de SMTP no envio
-    $mail->SMTPAuth = true; //Habilita a autenticação SMTP
-    $mail->Username   = 'dev.filipeandrade@gmail.com';
-    $mail->Password   = 'kyyf kpek gqsv oism';
-    // Criptografia do envio SSL também é aceito
-    $mail->SMTPSecure = 'tls';
-    // Informações específicadas pelo Google
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 587;
+    try{
+        $mail->isSMTP();        //Devine o uso de SMTP no envio
+        $mail->SMTPAuth = true; //Habilita a autenticação SMTP
+        $mail->Username   = 'dev.filipeandrade@gmail.com';
+        $mail->Password   = 'kyyf kpek gqsv oism';
+        // Criptografia do envio SSL também é aceito
+        $mail->SMTPSecure = 'tls';
+        // Informações específicadas pelo Google
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 587;
+    } catch (Exception $e){
+        $message -> setMessage("Erro no envio de Email", "error");
+    }
