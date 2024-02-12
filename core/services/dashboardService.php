88b683436e36120f7e686b89448766cdbee69e65
Filipe Andrade <filipeandrade.work@gmail.com>
2024-02-12 03:14:33 -0300

    php mailer configurado

--- core/services/dashboardService.php
+++ core/services/dashboardService.php
@@ -4,17 +4,18 @@ include_once("../../infrastructure/database.php");
 include_once("../../infrastructure/Message.php");
 include_once("../repositorys/OrderRepository.php");
 include_once("../entitys/Mail.php");
+require_once("../../infrastructure/mail.php");
 
 $message = new Message();
 
 $orderRepository = new OrderRepository($db_connection);
 
 if ($_POST["cancel"]) {
-    cancelProcess($_POST["cancel"], $orderRepository);
+    cancelProcess($_POST["cancel"], $orderRepository, $mail);
 } else if ($_POST["accept"]) {
-    acceptProcess($_POST["accept"], $orderRepository);
+    acceptProcess($_POST["accept"], $orderRepository, $mail);
 } else if ($_POST["post"]) {
-    postProcess($_POST["post"], $orderRepository);
+    postProcess($_POST["post"], $orderRepository, $mail);
 } else if ($_POST["delete"]) {
     deleteProcess($_POST["delete"], $orderRepository);
 } else if ($_POST["back"]) {
@@ -23,33 +24,64 @@ if ($_POST["cancel"]) {
     $message->setMessage("Processo não identificado", "error");
 }
 
-function cancelProcess($orderId, OrderRepository $orderRepository){
+function cancelProcess($orderId, OrderRepository $orderRepository, $mail){
     $orderRepository->deleteOrder($orderId);
     $order = $orderRepository -> findById($orderId);
     $to = $order -> getEmail();
-    $mail = new Mail($to, "Seu Pedido Foi Cancelado", "Mensagem de Exemplo");
-    $mail -> sendMail();
-    echo "oi";
+
+    // Define o remetente
+    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+    // Define o destinatário
+    $mail->addAddress($to, 'Destinatário');
+    // Conteúdo da mensagem
+    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+    $mail->Subject = 'Seu Pedido Foi Cancelado';
+    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+    // Enviar
+    $mail->send();
+
     header("Location: " . "../../dashboard.php");
 }
 
-function acceptProcess($orderId, OrderRepository $orderRepository){
+function acceptProcess($orderId, OrderRepository $orderRepository, $mail){
     $orderRepository->updateOrderStatus($orderId, "processamento");
     $order = $orderRepository -> findById($orderId);
+
     $to = $order -> getEmail();
-    $mail = new Mail($to, "Seu Pedido Foi Confirmado", "Mensagem de Exemplo");
-    $mail -> sendMail();
-    echo "oi";
+
+    // Define o remetente
+    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+    // Define o destinatário
+    $mail->addAddress($to, 'Destinatário');
+    // Conteúdo da mensagem
+    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+    $mail->Subject = 'Seu Pedido Foi Aprovado';
+    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+    // Enviar
+    $mail->send();
+
     header("Location: " . "../../dashboard.php");
 }
 
-function postProcess($orderId, OrderRepository $orderRepository){
+function postProcess($orderId, OrderRepository $orderRepository, $mail){
     $orderRepository->updateOrderStatus($orderId, "postado");
     $order = $orderRepository -> findById($orderId);
     $to = $order -> getEmail();
-    $mail = new Mail($to, "Seu Pedido Foi Postado", "Mensagem de Exemplo");
-    $mail -> sendMail();
-    echo "oi";
+
+    // Define o remetente
+    $mail->setFrom('dev.filipeandrade@gmail.com', 'Filipe Andrade');
+    // Define o destinatário
+    $mail->addAddress($to, 'Destinatário');
+    // Conteúdo da mensagem
+    $mail->isHTML(true);  // Seta o formato do e-mail para aceitar conteúdo HTML
+    $mail->Subject = 'Seu Pedido Foi Postado';
+    $mail->Body    = 'Este é o corpo da mensagem <b>Olá em negrito!</b>';
+    $mail->AltBody = 'Este é o corpo da mensagem para clientes de e-mail que não reconhecem HTML';
+    // Enviar
+    $mail->send();
+
     header("Location: " . "../../dashboard.php");
 }
 
---
+++ infrastructure/mail.php
@@ -0,0 +1,18 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    require 'path/to/PHPMailer/src/Exception.php';
+    require 'path/to/PHPMailer/src/PHPMailer.php';
+    require 'path/to/PHPMailer/src/SMTP.php';
+
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();        //Devine o uso de SMTP no envio
+    $mail->SMTPAuth = true; //Habilita a autenticação SMTP
+    $mail->Username   = 'dev.filipeandrade@gmail.com';
+    $mail->Password   = 'kyyf kpek gqsv oism';
+    // Criptografia do envio SSL também é aceito
+    $mail->SMTPSecure = 'tls';
+    // Informações específicadas pelo Google
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = 587;
